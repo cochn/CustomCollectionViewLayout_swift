58c533062b2d0562a0116b913b200e04b1ffd030
cochn <chenlove523@163.com>
2017-04-19 16:20:23 +0800

    基本框架

--- CustomCollectionViewLayout/ViewController.swift
+++ CustomCollectionViewLayout/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    var tableView: UITableView? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func createTableView(){
+        
+        tableView = UITableView.init(frame: CGRect.init(x: 0, y: 64, width: self.view.frame.size.width, height: self.view.frame.size.height - 64), style: UITableViewStyle.plain);
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        self.view.addSubview(tableView!)
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 5;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIden = "cellIden"
+        
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellIden)
+        if cell == nil {
+            cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: cellIden)
+        }
+        
+        
+        
+        return cell!
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
