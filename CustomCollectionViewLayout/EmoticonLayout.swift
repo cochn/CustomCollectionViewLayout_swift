2e87fd131fc1d44db9c753bf6aa01b98b7580ee9
曹臣 <caochen@caochendeiMac.local>
2017-07-07 09:21:05 +0800

    添加表情layout

---
+++ CustomCollectionViewLayout/EmoticonLayout.swift
@@ -0,0 +1,13 @@
+//
+//  EmoticonLayout.swift
+//  CustomCollectionViewLayout
+//
+//  Created by 曹臣 on 2017/7/6.
+//  Copyright © 2017年 曹臣. All rights reserved.
+//
+
+import UIKit
+
+class EmoticonLayout: UICollectionViewLayout {
+
+}
